0bf24544120cf3686c45b1b238b7487195316076
CindyvdVries <cindyvdvries@gmail.com>
2018-04-12 11:27:32 +0200

    minor changes

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/msfd-eutro/',
+    assetsPublicPath: '/MSFD-Eutro/',
 
     /**
      * Source Maps
